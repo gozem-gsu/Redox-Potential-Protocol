47ddc6597620e9a10feb73c52a0ff7e5b03b9cd7
selhajj1 <selhajj1@photonmgt.rs.gsu.edu>
2024-03-21 19:17:20 +0000

    Added direct opt for AQs Table5-c

---
+++ Table5/opt-direct/164/neutral/164.com
@@ -0,0 +1,8 @@
+%chk=164.chk
+%nproc=8
+%mem=30GB
+#P UB3LYP/6-311++G(d,p) nosym opt freq SCRF=(PCM,Solvent=n,n-DiMethylFormamide)
+
+Oxidized
+
+0 1
---
+++ Table5/opt-direct/2.1-check-negative-frequencies.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+
+def check_termination(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        last_line = lines[-1].strip() if lines else ''
+        
+        # Check if the file ended normally
+        if "Normal termination of Gaussian 16" in last_line:
+            freq_found = False
+            
+            # Parsing frequencies
+            for line in lines:
+                if line.startswith(' Frequencies'):
+                    freq_values = line.split()[2:5]
+                    freq_values = [float(freq) for freq in freq_values]
+                    freq_found = True
+                    break
+
+            if freq_found:
+                if all(freq > 0 for freq in freq_values):
+                    return True
+                else:
+                    return False
+        else:
+            return False
+
+
+for folder in [f"{i:03d}" for i in range(117,183)]:
+    for subdir in ['neutral', 'radical', 'anion']:
+        log_file_name = f"{folder}-an.log" if subdir == 'anion' else f"{folder}-rad.log" if subdir == 'radical' else f"{folder}.log"
+        com_file_name = f"{folder}-an.com" if subdir == 'anion' else f"{folder}-rad.com" if subdir == 'radical' else f"{folder}.com"
+
+        log_file_path = os.path.join(folder, subdir, log_file_name)
+        com_file_path = os.path.join(folder, subdir, com_file_name)
+
+        # Check if log file exists in the folder
+        if os.path.exists(log_file_path):
+            if not check_termination(log_file_path):
+                print(f"Frequencies negative in {log_file_name}")
+
+
+
