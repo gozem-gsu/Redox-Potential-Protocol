5079e9eb7c4395342b17912f86b9e5540d5d4df5
selhajj1 <selhajj1@photonmgt.rs.gsu.edu>
2024-03-21 19:22:40 +0000

    Added opt-SM12 for Table5-c AQs

---
+++ Table5-continued/opt-SM12/6-extract-values.py
@@ -0,0 +1,60 @@
+import os
+import csv
+
+def extract_last_scf_done(log_file_path):
+    last_scf_done = None
+    with open(log_file_path, 'r') as file:
+        for line in file:
+            if 'SCF Done' in line:
+                last_scf_done = line.split()[4]
+    return last_scf_done
+
+def extract_last_thermal_correction(log_file_path):
+    last_thermal_correction = None
+    with open(log_file_path, 'r') as file:
+        for line in file:
+            if 'Thermal correction to Gibbs Free Energy=' in line:
+                last_thermal_correction = line.split()[6]
+    return last_thermal_correction
+def extract_total_energy(out_file_path):
+    last_total_energy = None
+    with open(out_file_path, 'r') as file:
+        for line in file:
+            if 'Total energy in the final basis set' in line:
+                last_total_energy = line.split()[8]
+    return last_total_energy
+    
+
+def process_log_files():
+    # Create the 'values' directory if it doesn't exist
+    if not os.path.exists('values'):
+        os.makedirs('values')
+
+    for i in range(183,227):
+        folder = f"{i:03d}"
+
+        # Save each CSV file in the 'values' folder
+        csv_file_path = os.path.join('values', f'{folder}.csv')
+
+        with open(csv_file_path, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Protonation State', 'SCFE(g)', 'Thermal Correction to Gibss Free Energy', 'SCFE(sol)'])
+
+            for subdir in ['radical', 'neutral', 'anion']:
+                log_file_name = f"{folder}-an.log" if subdir == 'anion' else f"{folder}-rad.log" if subdir == 'radical' else f"{folder}.log" 
+                log_file_path = os.path.join(folder, subdir, log_file_name)
+
+                SM12_out_file_name = f"{folder}-an-SM12.out" if subdir == 'anion' else f"{folder}-rad-SM12.out" if subdir == 'radical' else f"{folder}-SM12.out" 
+                SM12_out_file_path = os.path.join(folder, subdir, SM12_out_file_name)
+
+                if os.path.exists(log_file_path):
+                    print(f"Extracting values from log file: {log_file_path}")
+                    scf_done_value = extract_last_scf_done(log_file_path)
+                    thermal_correction_value = extract_last_thermal_correction(log_file_path)
+                    SM12_scf_done_value = extract_total_energy(SM12_out_file_path)
+
+                    writer.writerow([subdir, scf_done_value, thermal_correction_value, SM12_scf_done_value])
+                else:
+                    print(f"No log file found in {log_file_path}")
+
+process_log_files()
---
+++ Table5-continued/opt-SM12/qchem.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH --job-name=JOBNAME
+#SBATCH -t 48:00:00
+#SBATCH -A TG-CHE180027
+#SBATCH --output="oqchem.%j"
+#SBATCH --error="eqchem.%j"
+#SBATCH --partition="shared"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --export=ALL
+
+module load cpu/0.15.4 gcc/10.2.0 mvapich2/2.3.6
+module load qchem/5.4
+
+export MKL_NUM_THREADS=8
+QCTHREADS=8
+export QCSCRATCH=/expanse/lustre/scratch/$USER/temp_project/qcscratch/$SLURM_JOB_ID
+mkdir -p $QCSCRATCH
+
+qchem -nt 8 $SLURM_JOB_NAME.in $SLURM_JOB_NAME.out
+
+rm -r $QCSCRATCH
