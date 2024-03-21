6af2df9fb24cd8bf884a097c0df136907983fbcd
selhajj1 <selhajj1@photonmgt.rs.gsu.edu>
2024-03-21 19:08:13 +0000

    added file.csv and needed scripts

---
+++ Table2-continued/check_Hs.py
@@ -0,0 +1,36 @@
+import os
+
+# Set the path to your directory containing the .xyz files
+directory = 'xyzs'
+
+# Looping from 0 to 60 (to include files named 000.xyz to 060.xyz)
+for i in range(61,116):
+    # Constructing the file name by padding the number with zeros
+    file_name = f'{i:03}.xyz'
+    file_path = os.path.join(directory, file_name)
+    print(file_path)
+    try:
+        # Opening the file
+        with open(file_path, 'r') as file:
+            lines = file.readlines()  # Read all lines in the file
+
+            # Check if the file has more than 3 lines
+            if len(lines) > 3:
+                has_H = False  # Flag to check if 'H' is found in the first column
+
+                # Looping through each line in the file
+                for line in lines:
+                    # Splitting the line into columns and checking the first column
+                    if line.strip():  # Making sure the line is not empty
+                        columns = line.split()
+                        if columns[0] == 'H':
+                            has_H = True
+                            break  # Stop checking this file if 'H' is found
+                print(has_H)
+                # If 'H' is not found in any line, print the file name
+                if not has_H:
+                    print(f'File with more than 3 lines and without H in the first column: {file_name}')
+
+    except FileNotFoundError:
+        # If the file is not found, print a message
+        print(f'File not found: {file_name}')
---
+++ Table2-continued/check_iodine.py
@@ -0,0 +1,29 @@
+import os
+
+directory = "xyzs"
+
+for i in range(61,116):
+    file_name = f"{i:03}.xyz"
+    file_path = os.path.join(directory, file_name)
+
+    try:
+        # Opening the file
+        with open(file_path, "r") as file:
+            lines = file.readlines()
+            if len(lines) > 3:
+                has_H = False
+                for line in lines:
+                    if line.strip():
+                        columns = line.split()
+
+                        if columns[0] == "I":
+                            has_H = True
+                            break
+                if has_H:
+                    print(f"File with Iodine: {file_name}")
+
+    except FileNotFoundError:
+        print(f"File not found: {file_name}")
+
+
+
