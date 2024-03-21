4ec983fa98280646a6e73be8700f0fb3e8f8081b
selhajj1 <selhajj1@photonmgt.rs.gsu.edu>
2024-03-21 19:04:44 +0000

    Added cosmo Table2-continued for BQs

---
+++ Table2-continued/opt-cosmo/4-solvation-energies-COSMO.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+for i in {061..115}; do
+    cat top-3 xyz-opt/$i-opt.xyz bottom >$i/neutral/$i-COSMO.in
+    sed -i "s/JOBNAME/$i-COSMO/" $i/neutral/$i-COSMO.in
+    sed -i '$a\\' $i/neutral/$i-COSMO.in
+    sed -i '$a\\' $i/neutral/$i-COSMO.in
+    sed -i '$a\\' $i/neutral/$i-COSMO.in
+    sed -i '$a\\' $i/neutral/$i-COSMO.in
+    sed -i '$a\\' $i/neutral/$i-COSMO.in
+    cp qchem.sh $i/neutral/
+    sed -i "s/JOBNAME/$i-COSMO/" $i/neutral/qchem.sh
+done
+for i in {061..115}; do
+    cat top-4 xyz-opt/$i-opt-rad.xyz bottom >$i/radical/$i-rad-COSMO.in
+    sed -i "s/JOBNAME/$i-rad-COSMO/" $i/radical/$i-rad-COSMO.in
+    sed -i '$a\\' $i/radical/$i-rad-COSMO.in
+    sed -i '$a\\' $i/radical/$i-rad-COSMO.in 
+    sed -i '$a\\' $i/radical/$i-rad-COSMO.in
+    sed -i '$a\\' $i/radical/$i-rad-COSMO.in
+    sed -i '$a\\' $i/radical/$i-rad-COSMO.in
+    cp qchem.sh $i/radical/
+    sed -i "s/JOBNAME/$i-rad-COSMO/" $i/radical/qchem.sh
+done
+for i in {061..115}; do
+    cat top-5 xyz-opt/$i-opt-an.xyz bottom >$i/anion/$i-an-COSMO.in
+    sed -i "s/JOBNAME/$i-an-COSMO/" $i/anion/$i-an-COSMO.in
+    sed -i '$a\\' $i/anion/$i-an-COSMO.in
+    sed -i '$a\\' $i/anion/$i-an-COSMO.in
+    sed -i '$a\\' $i/anion/$i-an-COSMO.in
+    sed -i '$a\\' $i/anion/$i-an-COSMO.in
+    sed -i '$a\\' $i/anion/$i-an-COSMO.in
+    cp qchem.sh $i/anion/
+    sed -i "s/JOBNAME/$i-an-COSMO/" $i/anion/qchem.sh
+done
+for i in {061..115}; do
+    cd $i/neutral
+    sbatch qchem.sh
+    cd ../../
+done
+for i in {061..115}; do
+    cd $i/radical
+    sbatch qchem.sh
+    cd ../../
+done
+for i in {061..115}; do
+    cd $i/anion
+    sbatch qchem.sh
+    cd ../../
+done
+
---
+++ Table2-continued/opt-cosmo/6-extract-values.py
@@ -0,0 +1,61 @@
+import os
+import csv
+
+def extract_last_scf_done(log_file_path):
+    last_scf_done = 0
+    with open(log_file_path, 'r') as file:
+        for line in file:
+            if 'SCF Done' in line:
+                last_scf_done = line.split()[4]
+    return last_scf_done
+
+def extract_last_thermal_correction(log_file_path):
+    last_thermal_correction = 0
+    with open(log_file_path, 'r') as file:
+        for line in file:
+            if 'Thermal correction to Gibbs Free Energy=' in line:
+                last_thermal_correction = line.split()[6]
+    return last_thermal_correction
+def extract_total_energy(out_file_path):
+    last_total_energy = 0
+    with open(out_file_path, 'r') as file:
+        for line in file:
+            if 'SCF   energy in the final basis set' in line:
+                last_total_energy = line.split()[8]
+    return last_total_energy
+    
+
+def process_log_files():
+    # Create the 'values' directory if it doesn't exist
+    if not os.path.exists('values'):
+        os.makedirs('values')
+
+    for i in range(61,116):
+        folder = f"{i:03d}"
+
+        # Save each CSV file in the 'values' folder
+        csv_file_path = os.path.join('values', f'{folder}.csv')
+
+        with open(csv_file_path, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Protonation State', 'SCFE(g)', 'Thermal Correction to Gibss Free Energy', 'SCFE(sol)'])
+
+            for subdir in ['radical', 'neutral', 'anion']:
+                log_file_name = f"{folder}-an.log" if subdir == 'anion' else f"{folder}-rad.log" if subdir == 'radical' else f"{folder}.log" 
+                log_file_path = os.path.join(folder, subdir, log_file_name)
+
+                COSMO_out_file_name = f"{folder}-an-COSMO.out" if subdir == 'anion' else f"{folder}-rad-COSMO.out" if subdir == 'radical' else f"{folder}-COSMO.out" 
+                COSMO_out_file_path = os.path.join(folder, subdir, COSMO_out_file_name)
+
+                if os.path.exists(log_file_path):
+                    print(f"Extracting values from log file: {log_file_path}")
+                    scf_done_value = extract_last_scf_done(log_file_path)
+                    thermal_correction_value = extract_last_thermal_correction(log_file_path)
+                    COSMO_scf_done_value = extract_total_energy(COSMO_out_file_path)
+
+                    writer.writerow([subdir, scf_done_value, thermal_correction_value, COSMO_scf_done_value])
+                else:
+                    print(f"No log file found in {log_file_path}")
+
+process_log_files()
+
