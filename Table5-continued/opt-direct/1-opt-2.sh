889c758fbcfcfa79ff063d05dda72af560c1684b
selhajj1 <selhajj1@photonmgt.rs.gsu.edu>
2024-03-21 19:23:26 +0000

    Added direct opt for Table5-c AQs

---
+++ Table5-continued/opt-direct/1-opt-2.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+for i in {183..226}; do mkdir $i; done
+
+for i in {183..226}; do    
+    mkdir $i/neutral/
+    cat top-0 xyz-opt/$i-opt.xyz >$i/neutral/$i.com
+    sed -i "s/JOBNAME/$i/" $i/neutral/$i.com
+    sed -i '$a\\' $i/neutral/$i.com
+    sed -i '$a\\' $i/neutral/$i.com
+    cp gaussian.sh $i/neutral/
+    sed -i "s/JOBNAME/$i/" $i/neutral/gaussian.sh
+done
+
+for i in {183..226}; do
+    mkdir $i/radical/
+    cat top-1 xyz-opt/$i-opt-rad.xyz >$i/radical/$i-rad.com
+    sed -i "s/JOBNAME/$i-rad/" $i/radical/$i-rad.com
+    sed -i '$a\\' $i/radical/$i-rad.com
+    sed -i '$a\\' $i/radical/$i-rad.com
+    cp gaussian.sh $i/radical/
+    sed -i "s/JOBNAME/$i-rad/" $i/radical/gaussian.sh
+done
+
+for i in {183..226}; do
+    mkdir $i/anion/
+    cat top-2 xyz-opt/$i-opt-an.xyz >$i/anion/$i-an.com
+    sed -i "s/JOBNAME/$i-an/" $i/anion/$i-an.com
+    sed -i '$a\\' $i/anion/$i-an.com
+    sed -i '$a\\' $i/anion/$i-an.com
+    cp gaussian.sh $i/anion/
+    sed -i "s/JOBNAME/$i-an/" $i/anion/gaussian.sh
+done
+
+for i in {183..226}; do
+    cd $i/neutral
+    sbatch gaussian.sh
+    cd ../../
+done
+
+for i in {183..226}; do
+    cd $i/radical
+    sbatch gaussian.sh
+    cd ../../
+done
+
+for i in {183..226}; do
+    cd $i/anion
+    sbatch gaussian.sh
+    cd ../../
+done
+
+
+
---
+++ Table5-continued/opt-direct/209/neutral/209.com
@@ -0,0 +1,10 @@
+%chk=209.chk
+%nproc=8
+%mem=30GB
+#P UB3LYP/6-311++G(d,p) nosym opt freq SCRF=(PCM,Solvent=n,n-DiMethylFormamide)
+
+Oxidized
+
+0 1
+
+
