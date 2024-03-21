ba1c8a8572ba2420b74a87909758b50c7b3649d8
selhajj1 <selhajj1@photonmgt.rs.gsu.edu>
2024-03-21 18:59:50 +0000

    Added additional scripts for Table2 with file.csv

---
+++ Table2/1-retrieve-molecules.py
@@ -0,0 +1,77 @@
+import os
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import Draw
+import cirpy
+
+file_path = "file.csv"
+
+with open(file_path, "r") as file:
+    lines = file.readlines()
+
+extracted_data = []
+for line in lines:
+    parts = line.split()
+    column2 = parts[1]
+    column3 = parts[2] if len(parts) > 2 else "N/A"
+    extracted_data.append([column2, column3])
+    print([column2, column3])
+
+if not os.path.exists("xyzs"):
+    os.makedirs("xyzs")
+
+for index, chemical in enumerate(extracted_data):
+    print(index)
+    print(chemical[1])
+    if chemical[1] != "N/A":
+        print(chemical)
+        smiles = cirpy.resolve(chemical[1], "smiles")
+        if smiles:
+            mol = Chem.MolFromSmiles(smiles)
+
+            if mol:
+                mol = Chem.AddHs(mol)
+
+                AllChem.EmbedMolecule(mol, AllChem.ETKDG())
+
+                xyz = Chem.MolToMolBlock(mol)
+
+                conf = mol.GetConformer()
+
+                filename = f"xyzs/{index:03d}.xyz"
+
+                with open(filename, "w") as file:
+                    file.write("#P\n\n")
+                    file.write("Gaussian input prepared by ASE\n\n")
+                    file.write("0 1\n")
+                    for atom in mol.GetAtoms():
+                        pos = conf.GetAtomPosition(atom.GetIdx())
+
+                        file.write(
+                            f"{atom.GetSymbol():<2} {pos.x: >15.10f} {pos.y: >15.10f} {pos.z: >15.10f}\n"
+                        )
+                print(f"XYZ coordinates saved for {chemical[1]} as {filename}")
+
+            else:
+                print(f"Failed to generate XYZ coordinates for {chemical}")
+
+        else:
+            print(f"SMILES not found for {chemical}")
+            filename = f"xyzs/{index:03d}.xyz"
+            not_found_file = f"xyzs/not_found_file.log"
+
+            with open(filename, "w") as file:
+                file.write("\n")
+            with open(not_found_file, "a") as not_found_file:
+                not_found_file.write(str([f"{index:03d}"] + chemical))
+                not_found_file.write("\n")
+    else:
+        print(f"SMILES not found for {chemical}")
+        filename = f"xyzs/{index:03d}.xyz"
+        not_found_file = f"xyzs/not_found_file.log"
+
+        with open(filename, "w") as file:
+            file.write("\n")
+        with open(not_found_file, "a") as not_found_file:
+            not_found_file.write(str([f"{index:03d}"] + chemical))
+            not_found_file.write("\n")
