751fcd172c573193bf11a076225d779271a6f21c
selhajj1 <selhajj1@photonmgt.rs.gsu.edu>
2024-06-21 17:31:52 +0000

    Added opt-cpcm Table6

---
+++ Table6/opt-CPCM/4-solvation-energies-CPCM.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+for i in {227..291}; do
+    cat top-3 xyz-opt/$i-opt.xyz bottom >$i/neutral/$i-CPCM.in
+    sed -i "s/JOBNAME/$i-CPCM/" $i/neutral/$i-CPCM.in
+    sed -i '$a\\' $i/neutral/$i-CPCM.in
+    sed -i '$a\\' $i/neutral/$i-CPCM.in
+    sed -i '$a\\' $i/neutral/$i-CPCM.in
+    sed -i '$a\\' $i/neutral/$i-CPCM.in
+    sed -i '$a\\' $i/neutral/$i-CPCM.in
+    cp qchem.sh $i/neutral/
+    sed -i "s/JOBNAME/$i-CPCM/" $i/neutral/qchem.sh
+done
+for i in {227..291}; do
+    cat top-4 xyz-opt/$i-opt-rad.xyz bottom >$i/radical/$i-rad-CPCM.in
+    sed -i "s/JOBNAME/$i-rad-CPCM/" $i/radical/$i-rad-CPCM.in
+    sed -i '$a\\' $i/radical/$i-rad-CPCM.in
+    sed -i '$a\\' $i/radical/$i-rad-CPCM.in 
+    sed -i '$a\\' $i/radical/$i-rad-CPCM.in
+    sed -i '$a\\' $i/radical/$i-rad-CPCM.in
+    sed -i '$a\\' $i/radical/$i-rad-CPCM.in
+    cp qchem.sh $i/radical/
+    sed -i "s/JOBNAME/$i-rad-CPCM/" $i/radical/qchem.sh
+done
+for i in {227..291}; do
+    cat top-5 xyz-opt/$i-opt-an.xyz bottom >$i/anion/$i-an-CPCM.in
+    sed -i "s/JOBNAME/$i-an-CPCM/" $i/anion/$i-an-CPCM.in
+    sed -i '$a\\' $i/anion/$i-an-CPCM.in
+    sed -i '$a\\' $i/anion/$i-an-CPCM.in
+    sed -i '$a\\' $i/anion/$i-an-CPCM.in
+    sed -i '$a\\' $i/anion/$i-an-CPCM.in
+    sed -i '$a\\' $i/anion/$i-an-CPCM.in
+    cp qchem.sh $i/anion/
+    sed -i "s/JOBNAME/$i-an-CPCM/" $i/anion/qchem.sh
+done
+for i in {227..291}; do
+    cd $i/neutral
+    sbatch qchem.sh
+    cd ../../
+done
+for i in {227..291}; do
+    cd $i/radical
+    sbatch qchem.sh
+    cd ../../
+done
+for i in {227..291}; do
+    cd $i/anion
+    sbatch qchem.sh
+    cd ../../
+done
+
