a79bcfbd3335b5d4b314598294c78edc33d99564
selhajj1 <selhajj1@photonmgt.rs.gsu.edu>
2024-06-21 17:22:50 +0000

    Added opt-CPCM Table5

---
+++ Table5/opt-CPCM/1-opt-2.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+for i in {117..182}; do mkdir $i; done
+
+for i in {117..182}; do    
+    mkdir $i/neutral/
+    cat top-0 xyzs/$i-opt.xyz >$i/neutral/$i.com
+    sed -i "s/JOBNAME/$i/" $i/neutral/$i.com
+    sed -i '$a\\' $i/neutral/$i.com
+    sed -i '$a\\' $i/neutral/$i.com
+    cp gaussian.sh $i/neutral/
+    sed -i "s/JOBNAME/$i/" $i/neutral/gaussian.sh
+done
+
+for i in {117..182}; do
+    mkdir $i/radical/
+    cat top-1 xyzs/$i-opt-rad.xyz >$i/radical/$i-rad.com
+    sed -i "s/JOBNAME/$i-rad/" $i/radical/$i-rad.com
+    sed -i '$a\\' $i/radical/$i-rad.com
+    sed -i '$a\\' $i/radical/$i-rad.com
+    cp gaussian.sh $i/radical/
+    sed -i "s/JOBNAME/$i-rad/" $i/radical/gaussian.sh
+done
+
+for i in {117..182}; do
+    mkdir $i/anion/
+    cat top-2 xyzs/$i-opt-an.xyz >$i/anion/$i-an.com
+    sed -i "s/JOBNAME/$i-an/" $i/anion/$i-an.com
+    sed -i '$a\\' $i/anion/$i-an.com
+    sed -i '$a\\' $i/anion/$i-an.com
+    cp gaussian.sh $i/anion/
+    sed -i "s/JOBNAME/$i-an/" $i/anion/gaussian.sh
+done
+
+for i in {117..182}; do
+    cd $i/neutral
+    sbatch gaussian.sh
+    cd ../../
+done
+
+for i in {117..182}; do
+    cd $i/radical
+    sbatch gaussian.sh
+    cd ../../
+done
+
+for i in {117..182}; do
+    cd $i/anion
+    sbatch gaussian.sh
+    cd ../../
+done
+
+
+
---
+++ Table5/opt-CPCM/check-CPCM-termination.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+def check_termination(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        last_line = lines[-5].strip() if lines else ''
+        return "Thank you very much for using Q-Chem.  Have a nice day." in last_line
+
+# Loop through folders '000' to '060'
+for folder in [f"{i:03d}" for i in range(117,183)]:
+    for subdir in ['neutral', 'radical', 'anion']:
+        out_file_name = f"{folder}-an-CPCM.out" if subdir == 'anion' else f"{folder}-rad-CPCM.out" if subdir == 'radical' else f"{folder}-CPCM.out"
+
+        log_file_path = os.path.join(folder, subdir, out_file_name)
+
+        # Check if log file exists in the folder
+        if os.path.exists(log_file_path):
+            if not check_termination(log_file_path):
+                print(f"Warning: 'Thank you very much for using Q-Chem' not found in {log_file_path}")
+
+        else:
+            print(f"No log file found in {log_file_path}")
